f837539300f20087196a7b3002960cea1e5c631e
jamshid-python <100790769+jamshid-python@users.noreply.github.com>
2022-03-02 21:45:49 +0500

    Add files via upload

---
+++ kril_latin.py
@@ -0,0 +1,26 @@
+import telebot
+from translater import to_cyrillic, to_latin
+
+TOKEN = "5032688757:AAHYtv9Tmrh1ClSEr-4KeQPBRwBvlADJA-w"  # <-- Tokeningizni shu yerga yozing
+bot = telebot.TeleBot(token=TOKEN)
+
+# \start komandasi uchun mas'ul funksiya
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    username = (
+        message.from_user.username
+    )  # Bu usul bilan foydalanuvchi nomini olishimiz mumkin
+    xabar = f"Assalom alaykum, {username} Kirill-Lotin-Kirill botiga xush kelibsiz!"
+    xabar += "\nKrilcha yoki Lotincha Matningizni yuboring."
+    xabar += "\nCreator: Jamshid Ahmadov"
+    xabar += "\nMurojaat @FunnyAlone"
+    bot.reply_to(message, xabar)
+
+# matnlar uchun mas'ul funksiya
+@bot.message_handler(func=lambda msg: msg.text is not None)
+def translit(message):
+    msg = message.text
+    javob = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    bot.reply_to(message, javob(msg))
+
+bot.polling()
